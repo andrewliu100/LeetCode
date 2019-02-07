3265ecb534a413a04922b8670c68632dde2e1d25
Andrew Liu <andrewliu100@gmail.com>
2019-02-07 22:18:56 +1100

    Add Q11

---
+++ src/com/leetcode/ContainerWithMostWater.java
@@ -0,0 +1,41 @@
+package com.leetcode;
+
+/**
+ * 11. Container With Most Water
+ *
+ * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+ * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+ * Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+ *
+ * Note: You may not slant the container and n is at least 2.
+ */
+public class ContainerWithMostWater {
+
+    /**
+     * O(n^2)
+     */
+    public int maxAreaBruteForce(int[] height) {
+        int area = 0;
+        for (int i = 0; i < height.length; i++)
+            for (int j = i; j < height.length; j++)
+                area = Math.max(area, Math.min(height[i], height[j]) * (j - i));
+        return area;
+    }
+
+    /**
+     * O(n)
+     */
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1;
+        int area = 0;
+        while (i < j) {
+            area = Math.max(area, Math.min(height[i], height[j]) * (j - i));
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return area;
+    }
+}
